3051a9179edfefcfead3a1d05fb64d321fd6de01
iheb-khalfallah <ihebkhalfallah1@gmail.com>
2025-01-29 15:58:32 +0100

    Résolution de conflits v1

--- src/main/java/com/aegis/AegisDeviceManagement/repository/ProductRepository.java
+++ src/main/java/com/aegis/AegisDeviceManagement/repository/ProductRepository.java
@@ -5,9 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-<<<<<<< HEAD
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-=======
-public interface ProductRepository extends JpaRepository< Product, UUID > {
->>>>>>> fd2916cb76622af27e886c3de364282dc20bf0d6
+
 }
